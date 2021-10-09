7bb16be8ffaf10a351f897dbda5374d1b4ea6d96
jadessu <junioradessu@gmail.com>
2021-10-08 22:00:32 -0500

    add state for sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,55 @@
-import React from 'react'
-import styled from "styled-components"
-import {Link} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 
 const Nav = styled.div`
-background: #15171c;
-height: 80px;
-display: flex;
-justify-content: flex-start;
-align-items: center;
+  background: #15171c;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const SidebarNav = styled.nav`
+  background: #15171c;
+  width: 250px;
+  height: 100vh;
+`;
+const SidebarWrap = styled.div`
+  width: 100%;
 `;
 
 function Sidebar() {
-    return (
-       <>
-    <Nav>
-        <NavIcon to="#">
-            <FaIcons.FaBars/>
-        </NavIcon>
+    const [sidebar, setSidebar] = useState(false)
+const showSidebar = () => setSidebar(!sidebar)
 
-    </Nav>
-       </>
-    )
+  return (
+    <>
+      <Nav>
+        <NavIcon to="#">
+          <FaIcons.FaBars />
+        </NavIcon>
+      </Nav>
+      <SidebarNav>
+        <SidebarWrap>
+          <NavIcon to="#">
+            <AiIcons.AiOutlineClose />
+          </NavIcon>
+        </SidebarWrap>
+      </SidebarNav>
+    </>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
